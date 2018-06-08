47416c5cd5aa03a6ff031c2d6bb1dae5a98159be
Pamela Frieze <pfrieze@gmail.com>
2018-06-08 23:22:04 +0000

    automatically backed up by learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,11 @@
 class Movie < ActiveRecord::Base
-  attr_accessor :title, :release_date, :director
+  attr_accessor :title, :release_date, :director, :lead, :in_theaters
 
    def initialize
      @title = title
      @release_date = release_date
-     @director = director 
+     @director = director
+
    end
 
 end
