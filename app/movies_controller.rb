9b6d682e04b7e5201de0dca2fd7f91262863a261
Pamela Frieze <pfrieze@gmail.com>
2018-06-08 23:55:20 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -4,13 +4,13 @@
 # def make_a_new_movie_instance    # def make_a_new_movie_instance
 #   movie = __                     #   movie = Movie.new
 # end                              # end
-
+require 'pry'
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
   movie.save
 end
-binding.pry 
+binding.pry
 def can_be_created_with_a_hash_of_attributes
   attributes = {
       title: "The Sting",
