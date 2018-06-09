3b41fe54207ba0dd0ebcd55deb850b6f497d73ad
Pamela Frieze <pfrieze@gmail.com>
2018-06-09 00:25:05 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -26,8 +26,8 @@ end
 
 def can_be_created_in_a_block(title = nil, release_date = nil)
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = title
+    m.release_date = release_date
   end
 end
 
